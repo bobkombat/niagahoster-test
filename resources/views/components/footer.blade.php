09ed1d98e1a55b85a1c3eaa42e84ce040df58d88
bobkombat <rdhwnrmdhny@gmail.com>
2020-11-26 02:12:14 +0700

    add: done layouting -responsive

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,101 @@
-<div class="container mx-auto justify-content-between mt-5 text-tri">
-    <div></div>
-    <div></div>
+<div class="container mx-auto justify-content-between mt-5 mb-5 text-footer">
+    <div class="d-flex justify-content-between text-left">
+        <div class="w-25 mb-5 pr-4">
+            <h6 class="mb-4 text-footer-head">HUBUNGI KAMI</h6>
+            <p class="text-footer-body">0274-5305505</p>
+            <p class="text-footer-body">Senin - Minggu</p>
+            <p class="text-footer-body">24 Jam Nonstop</p>
+            <br>
+            <p class="text-footer-body">Jl. Selokan Mataram Monjali</p>
+            <p class="text-footer-body">Karangjati MT I/304</p>
+            <p class="text-footer-body">Sinduadi, Mlati, Sleman</p>
+            <p class="text-footer-body">Yogyakarta 55284</p>
+        </div>
+        <div class="w-25 mb-5 pr-4">
+            <h6 class="mb-4 text-footer-head">LAYANAN</h6>
+            <p class="text-footer-body">Domain</p>
+            <p class="text-footer-body">Shared Hosting</p>
+            <p class="text-footer-body">Cloud VPS Hosting</p>
+            <p class="text-footer-body">Managed VPS Hosting</p>
+            <p class="text-footer-body">Web Builder</p>
+            <p class="text-footer-body">Keamanan SSL / HTTPS</p>
+            <p class="text-footer-body">Jasa Pembuatan Website</p>
+            <p class="text-footer-body">Program Affiliasi</p>
+        </div>
+        <div class="w-25 mb-5 pr-4">
+            <h6 class="mb-4 text-footer-head">SERVICE HOSTING</h6>
+            <p class="text-footer-body">Hosting Murah</p>
+            <p class="text-footer-body">Hosting Indonesia</p>
+            <p class="text-footer-body">Hosting Singapura SG</p>
+            <p class="text-footer-body">Hosting PHP</p>
+            <p class="text-footer-body">Hosting Wordpress</p>
+            <p class="text-footer-body">Hosting Laravel</p>
+        </div>
+        <div class="w-25 mb-5 pr-4">
+            <h6 class="mb-4 text-footer-head">TUTORIAL</h6>
+            <p class="text-footer-body">Knowledgebase</p>
+            <p class="text-footer-body">Blog</p>
+            <p class="text-footer-body">Cara Pembayaran</p>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center text-left">
+        <div class="w-25 mb-5 pr-4">
+            <h6 class="mb-4 text-footer-head">TENTANG KAMI</h6>
+            <p class="text-footer-body">Tim Niagahoster</p>
+            <p class="text-footer-body">Karir</p>
+            <p class="text-footer-body">Events</p>
+            <p class="text-footer-body">Penawaran & Promo Spesial</p>
+            <p class="text-footer-body">Kontak Kami</p>
+        </div>
+        <div class="w-25 mb-5 pr-4">
+            <h6 class="mb-4 text-footer-head">KENAPA PILIH NIAGAHOSTER?</h6>
+            <p class="text-footer-body">Support Terbaik</p>
+            <p class="text-footer-body">Garansi Harga Termurah</p>
+            <p class="text-footer-body">Domain Gratis Selamanya</p>
+            <p class="text-footer-body">Datacenter Hosting Terbaik</p>
+            <p class="text-footer-body">Review Pelanggan</p>
+        </div>
+        <div class="w-25 mb-5 pr-4">
+            <h6 class="mb-4 text-footer-head">NEWSLETTER</h6>
+            <div class="w-100 rounded-pill d-flex justify-content-between align-items-center" style="height: 2.5rem; background-color: #ffffff">
+                <form action="" method="POST" class="w-100 d-flex justify-content-between">
+                    <input class="text-tri ml-3" type="email" style="border: none;" autocomplete="off" placeholder="Email" name="email">
+                    <input type="submit" class="btn btn-primary rounded-pill btn-sm mr-2" value="Berlangganan">
+                </form>
+            </div>
+            <p class="text-footer-body">Dapatkan promo dan konten menarik dari penyedia hosting terbaik Anda.</p>
+        </div>
+        <div class="w-25">
+            <h6 class="mb-4 text-footer-head">&MediumSpace;</h6>
+            <div class="d-flex justify-content-between">
+                <button style="width: 4rem; height: 4rem;" class="btn btn-outline-light rounded-pill">F</button>
+                <button style="width: 4rem; height: 4rem;" class="btn btn-outline-light rounded-pill">T</button>
+                <button style="width: 4rem; height: 4rem;" class="btn btn-outline-light rounded-pill">G</button>
+            </div>
+        </div>
+    </div>
+    <div class="w-100 pb-4 mb-3 border-bottom">
+        <h6 class="mb-4 text-footer-head">PEMBAYARAN</h6>
+        <div class="d-flex mb-4">
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+            <div class="mr-2 rounded-lg border text-footer-body" style="height: 3rem; width: 5rem;">BANK</div>
+        </div>
+        <p class="text-footer-body">Aktivasi instan dengan e-Payment. Hosting dan domain langsung aktif!</p>
+    </div>
+    <div class="d-flex justify-content-between">
+        <div >
+            <p class="text-footer-body">Copyright &copy;2016 Niagahoster | Hosting powered by PHP7, CloudLinux, CloudFlare, BitNinja and DC Biznet Technovillage Jakarta</p>
+            <p class="text-footer-body">Cloud VPS Murah powered by Webuzo Softlaculous, Intel SSD and cloud computing technology</p>
+        </div>
+        <div class="text-footer-body">
+            <a href="">Syarat dan Ketentuan</a> | <a href="">Kebijakan Privasi</a>
+        </div>
+    </div>
 </div>
